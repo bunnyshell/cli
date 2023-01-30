3dec7910d40c024e4a3ff4d84615d1aef85af20b
Cristian Lupu <cristian.lupu@bunnyshell.com>
2023-01-30 17:50:13 +0200

    Allowing HAL links in paginated output for json/yaml

--- pkg/formatter/formatter.go
+++ pkg/formatter/formatter.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 
-	"bunnyshell.com/sdk"
 	"gopkg.in/yaml.v3"
 )
 
@@ -14,23 +13,6 @@ var errUnknownFormat = errors.New("unknown format")
 func Formatter(data interface{}, format string) ([]byte, error) {
 	if format == "stylish" {
 		return stylish(data)
-	}
-
-	// not really useful
-	// maybe update the hal spec ?
-	switch t := data.(type) {
-	case *sdk.PaginatedOrganizationCollection:
-		t.Links = nil
-	case *sdk.PaginatedProjectCollection:
-		t.Links = nil
-	case *sdk.PaginatedEnvironmentCollection:
-		t.Links = nil
-	case *sdk.PaginatedComponentCollection:
-		t.Links = nil
-	case *sdk.PaginatedEventCollection:
-		t.Links = nil
-	case *sdk.PaginatedEnvironmentVariableCollection:
-		t.Links = nil
 	}
 
 	switch format {
