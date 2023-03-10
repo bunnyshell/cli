e60784662624b93975cbd4c9b41c38eca4814355
Cristian Lupu <cristian.lupu@bunnyshell.com>
2023-03-10 13:36:19 +0200

    Fix profile name autocomplete

--- pkg/config/manager.cobra.go
+++ pkg/config/manager.cobra.go
@@ -42,11 +42,7 @@ func (manager *Manager) profileNamesCompletion() ShellCompletion {
 	return func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		MainManager.Load()
 
-		if MainManager.Error != nil {
-			return []string{}, cobra.ShellCompDirectiveDefault
-		}
-
-		return MainManager.config.profileNames(), cobra.ShellCompDirectiveDefault
+		return MainManager.config.profileNames(), cobra.ShellCompDirectiveNoFileComp
 	}
 }
 
