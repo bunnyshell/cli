2dd8a9ca3cb7d0568a1090a32336a589698e7777
Amin Al Ali Al Darwish <amin@bunnyshell.com>
2024-05-21 00:23:21 +0300

    updated post checking if isSecret defaults to false|null

--- cmd/variable/action/import.go
+++ cmd/variable/action/import.go
@@ -57,26 +57,16 @@ func init() {
 
 			if len(data.Vars) > 0 {
 				for key, value := range data.Vars {
-					err := createEnvVar(cmd, key, value, false)
-					if err != nil {
-						if ignoreDuplicates && err.Error() == "An error occurred: name: An Environment Variable with this name already exists in this environment." {
-							continue
-						} else {
-							return lib.FormatCommandError(cmd, err)
-						}
+					if err := createEnvVar(cmd, key, value, false, ignoreDuplicates); err != nil {
+						return lib.FormatCommandError(cmd, err)
 					}
 				}
 			}
 
 			if len(data.Secrets) > 0 {
 				for key, value := range data.Secrets {
-					err := createEnvVar(cmd, key, value, true)
-					if err != nil {
-						if ignoreDuplicates && err.Error() == "An error occurred: name: An Environment Variable with this name already exists in this environment." {
-							continue
-						} else {
-							return lib.FormatCommandError(cmd, err)
-						}
+					if err := createEnvVar(cmd, key, value, true, ignoreDuplicates); err != nil {
+						return lib.FormatCommandError(cmd, err)
 					}
 				}
 			}
@@ -121,23 +111,27 @@ func readFile(fileName string, data *map[string]string) error {
 	return nil
 }
 
-func createEnvVar(cmd *cobra.Command, name string, value string, isSecret bool) error {
+func createEnvVar(cmd *cobra.Command, name string, value string, isSecret bool, ignoreDuplicates bool) error {
 	settings := config.GetSettings()
 	createOptions := variable.NewCreateOptions()
 	createOptions.Environment = settings.Profile.Context.Environment
 	createOptions.Name = name
 	createOptions.Value = value
-	if isSecret {
+	if isSecret  {
 		createOptions.IsSecret = enum.BoolTrue
 	}
 
 	model, err := variable.Create(createOptions)
 
-	if err != nil {
-		return err
-	} else {
+	if err == nil {
 		lib.FormatCommandData(cmd, model)
+
+		return nil
+	} 
+
+	if ignoreDuplicates && err.Error() == "An error occurred: name: An Environment Variable with this name already exists in this environment." {
+		return nil
 	}
 
-	return nil 
+	return err
 }
